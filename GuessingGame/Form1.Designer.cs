12a03d1147de256f3be09851188b3f862f5a7cbf
Maqsood Arain <a.a.arain@hotmail.com>
2022-03-17 16:08:51 -0400

    Second commit. The code works.

--- GuessingGame/Form1.Designer.cs
+++ GuessingGame/Form1.Designer.cs
@@ -36,37 +36,39 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(15, 52);
+            this.textBox1.Location = new System.Drawing.Point(12, 64);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(12, 18);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(222, 26);
             this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.label1.Text = "I Have Thought of A Number Between 1 - 20.\r\nCan You Guess What It Is?";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(12, 110);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(19, 13);
             this.label2.TabIndex = 2;
-            this.label2.Text = "label2";
+            this.label2.Text = "....";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(156, 52);
+            this.button1.Location = new System.Drawing.Point(145, 64);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 3;
-            this.button1.Text = "button1";
+            this.button1.Text = "Guess";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
--- GuessingGame/Form1.cs
+++ GuessingGame/Form1.cs
@@ -12,9 +12,34 @@ namespace GuessingGame
 {
     public partial class Form1 : Form
     {
+        private int num;
+        public void setNum( int num ) { this.num = num; }
+        public int getNum() { return num; }
         public Form1()
         {
             InitializeComponent();
+            Random rnd = new Random();
+            setNum(rnd.Next(1, 21));
+        }
+
+        public void rate( int guess, int num)
+        {
+            if (guess < num) label2.Text = "Too Low!";
+            else
+                if (guess > num) label2.Text = "Too High!";
+            else
+                label2.Text = "*** Correct ***" ;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int guess = Convert.ToInt32(textBox1.Text);
+            rate(guess, getNum());
         }
     }
 }
